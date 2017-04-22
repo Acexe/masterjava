025c4383eae8c83b6766b55cbc0a0d8179ad370e
Acexe <antzub@ya.ru>
2017-04-22 18:11:45 +0300

    7 6 publish customizedWSDL

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServiceClient.java
@@ -7,17 +7,13 @@ import javax.xml.ws.Service;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * User: gkislin
- * Date: 28.05.2014
- */
 public class MailServiceClient {
 
     public static void main(String[] args) throws MalformedURLException {
         QName qname = new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService");
         Service service = Service.create(
                 new URL("http://localhost:8080/mail/mailService?wsdl"),
-                new QName("http://mail.service.masterjava.javaops.ru/", "MailServiceImplService"));
+                new QName("http://mail.javaops.ru/", "MailServiceImplService"));
 
         MailService mailService = service.getPort(MailService.class);
         mailService.sendMail(ImmutableList.of(new Addressee("acexe7@gmail.com", null)), null, "Subject", "Body");
