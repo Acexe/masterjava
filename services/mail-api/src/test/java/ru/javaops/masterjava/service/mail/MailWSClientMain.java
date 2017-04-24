511a8dcd34eac860120460833aa9428c11660a0f
Acexe <antzub@ya.ru>
2017-04-25 02:21:53 +0300

    8 3 HW7 update wsdl

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,11 +1,12 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
 
 public class MailWSClientMain {
     public static void main(String[] args) {
-        MailWSClient.sendMail(
-                ImmutableList.of(new Addressee("Anton <antzub@ya.ru>")),
-                ImmutableList.of(new Addressee("Мастер Java <acexe7@gmail.com>")), "Subject", "Body");
+        MailWSClient.sendToGroup(
+                ImmutableSet.of(new Addressee("Anton <antzub@ya.ru>")),
+                ImmutableSet.of(new Addressee("Мастер Java <acexe7@gmail.com>")), "Subject", "Body");
     }
 }
