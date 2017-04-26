ecee55811b36bf4459d0cb19c13f4b3d96c4871c
Acexe <antzub@ya.ru>
2017-04-27 00:23:22 +0300

    8 5 soap exceptions

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -2,11 +2,20 @@ package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
-
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
 public class MailWSClientMain {
     public static void main(String[] args) {
-        MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("Anton <antzub@ya.ru>")),
-                ImmutableSet.of(new Addressee("Мастер Java <acexe7@gmail.com>")), "Subject", "Body");
+        ImmutableSet<Addressee> addressees = ImmutableSet.of(
+                            new Addressee("antzub@ya.ru"),
+                            new Addressee("Мастер Java <acexe7@gmail.com>"),
+                            new Addressee("Bad Email <bad_email.ru>"));
+                try {
+                        String state = MailWSClient.sendToGroup(addressees, ImmutableSet.of(), "Subject", "Body");
+                       System.out.println(state);
+                   } catch (Throwable e) {
+                        log.error(e.toString(), e);
+                   }
     }
+
 }
