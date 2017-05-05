7a551b9f413f2504a7a0bf8e5af438f65ba0efec
Acexe <antzub@ya.ru>
2017-05-05 16:43:48 +0300

    9 1 HW8 service attach

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -3,19 +3,23 @@ package ru.javaops.masterjava.service.mail;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import lombok.extern.slf4j.Slf4j;
+
+import javax.activation.DataHandler;
+import java.io.File;
+
 @Slf4j
 public class MailWSClientMain {
     public static void main(String[] args) {
         ImmutableSet<Addressee> addressees = ImmutableSet.of(
-                            new Addressee("antzub@ya.ru"),
-                            new Addressee("Мастер Java <acexe7@gmail.com>"),
-                            new Addressee("Bad Email <bad_email.ru>"));
-                try {
-                        String state = MailWSClient.sendToGroup(addressees, ImmutableSet.of(), "Subject", "Body");
-                       System.out.println(state);
-                   } catch (Throwable e) {
-                        log.error(e.toString(), e);
-                   }
-    }
+                new Addressee("Мастер Java <acexe7@gmail.com>"));
 
+        try {
+            String state = MailWSClient.sendToGroup(addressees, ImmutableSet.of(), "Subject", "Body", ImmutableList.of(
+                    new Attach("version.html", new DataHandler(new File("config_templates/version.html").toURI().toURL()))
+            ));
+            System.out.println(state);
+        } catch (Throwable e) {
+            log.error(e.toString(), e);
+        }
+    }
 }
